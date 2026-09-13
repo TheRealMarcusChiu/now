61de09143ac073502282b410ea66aa96fcd7c637
TheRealMarcusChiu <marcuschiu9@gmail.com>
2026-09-13 10:37:09 -0500

    log: 2026-09-13T15:37:09.557Z

---
+++ data/days/2026-09-13.js
@@ -0,0 +1,1 @@
+__logEvent({"domain":"desiringgod.org","end":"2026-09-13T15:27:03.662Z","secs":113,"source":"chrome","title":"To Live Is Christ — What Does That Mean? | Desiring God","ts":"2026-09-13T15:25:10.595Z","type":"web","url":"https://www.desiringgod.org/interviews/to-live-is-christ-what-does-that-mean"});
